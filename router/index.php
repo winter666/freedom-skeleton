4e4b3389493f82424af62dac9118db643bfec87e
Davut <sergeevdavut@mail.ru>
2022-11-19 21:44:18 +0300

    Route for controller docs

--- router/index.php
+++ router/index.php
@@ -36,6 +36,15 @@ Router::get('/api-docs/mvc/router', function () {
     return $render;
 });
 
+Router::get('/api-docs/mvc/controller', function () {
+    $render = Layout::view('main',
+        ['page' => 'pages.docs.index', 'doc_chapter' => 'pages.docs.mvc.controller'],
+        ['title' => 'Controller'],
+    );
+    $render->addCss('head', 'css.docs');
+    return $render;
+});
+
 Router::fallback(function () {
     return Layout::view('main', [
         'page' => 'pages.404',
